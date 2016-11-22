a3643e365001e6761cefbdc9d643012fe2da653e
유동호 <deaocis@gmail.com>
2016-11-22 14:49:52 +0900

    Sibal

--- ConsoleApplication1/ConsoleApplication1/source.cpp
+++ ConsoleApplication1/ConsoleApplication1/source.cpp
@@ -1,386 +1,61 @@
-#include <stdio.h>
-#include <Windows.h>
-#include <conio.h>
 #include <iostream>
 
-#define SCR_WIDTH 40
-#define SCR_HEIGHT 20
-#define MAX_BULLETS 20
+using namespace std;
 
-typedef struct
-{
-	int state;
-	int width;
-	int height;
+#define random_data 100000
 
-	int pos_x;
-	int pos_y;
-
-	char *image_;
-}Object;
-
-class GeometricObject
+class hypothesis
 {
 public:
-	virtual void draw()
+	double a_, b_;
+
+	hypothesis()
+		:a_(0.0),b_(0.0)
+	{}
+
+	double GetY(const double& x_data)
 	{
-		std::cout << "Error!" << std::endl;
+		return a_*x_data + b_+ (double)(rand() / RAND_MAX);
 	}
 };
-
-class Player :public GeometricObject
-{
-public:
-	int state;
-	int width;
-	int height;
-
-	int pos_x;
-	int pos_y;
-
-	char *image_;
-
-	Player(const int& _width, const int& _height, 
-			const int& _pos_x, const int& _pos_y, char *_image_)
-	{
-		initialize(_width, _height, _pos_x, _pos_y,_image_);
-	}
-
-	void initialize(const int& _width, const int& _height,
-					const int& _pos_x, const int& _pos_y,char *_image_)
-	{
-		width = _width;
-		height = _height;
-		pos_x = _pos_x;
-		pos_y = _pos_y;
-		image_ = _image_;
-	}
-
-	void draw()
-	{
-		drawToBackBuffer(pos_x, pos_y, image_);
-	}
-};
-
-class Enemy :public GeometricObject
-{
-	int state;
-	int width;
-	int height;
-
-	int pos_x;
-	int pos_y;
-
-	char *image_;
-
-	Enemy(const int& _state, const int& _width, const int& _height,
-		const int& _pos_x, const int& _pos_y, char *_image_)
-	{
-		initialize(_state, _width, _height, _pos_x, _pos_y, _image_);
-	}
-
-	void initialize(const int& _state, const int& _width, const int& _height,
-		const int& _pos_x, const int& _pos_y, char *_image_)
-	{
-		state = _state;
-		width = _width;
-		height = _height;
-		pos_x = _pos_x;
-		pos_y = _pos_y;
-		image_ = _image_;
-	}
-
-	void draw()
-	{
-		drawToBackBuffer(pos_x, pos_y, image_);
-	}
-};
-
-//Object player;
-//Object enemy;
-
-Object *p_bullet_array[MAX_BULLETS];
-//Object *p_bullet = NULL;	
-
-int score = 0;
-
-char front_buffer[SCR_HEIGHT][SCR_WIDTH];
-char back_buffer[SCR_HEIGHT][SCR_WIDTH];
-
-void moveCursorTo(const short x, const short y);
-void drawToBackBuffer(const int i, const int j, char* image);
-void render();
-void shootBullet();
-void drawBoundary();
-void drawAll();
-
-GeometricObject **my_objects = new GeometricObject*[2];
-
-void draw_PlayerAndEnemy()
-{
-	for (int i = 0; i < 2; i++)
-		my_objects[i]->draw();
-}
 
 int main()
 {
-	for (int j = 0; j < SCR_HEIGHT; j++)
-		for (int i = 0; i < SCR_WIDTH; i++)
-		{
-			front_buffer[j][i] = '\0';
-			back_buffer[j][i] = '\0';
-		}
-	
-	CONSOLE_CURSOR_INFO cur_info;
-	cur_info.dwSize = 1;
-	cur_info.bVisible = false;
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cur_info);
+	hypothesis lh;
 
-	//initialize bullet
-	for (int b = 0; b < MAX_BULLETS; b++)
+	double* x_data = new double[random_data];
+	double* y_data = new double[random_data];
+
+	for (int i = 0; i < random_data; i++)
 	{
-		p_bullet_array[b] = NULL;
-	}
-	
-	//initialize Player
-	my_objects[0] = new Player(5, 1, 10, 3, "<-O->");
-
-	//initialize Enemy
-	my_objects[1] = new Enemy(0, 5, 1, 15, SCR_HEIGHT - 3, "(O_O)\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<\0>XoX<");
-
-	while (1)    //main game loop
-	{
-		if (GetAsyncKeyState(VK_LEFT) & 0x8000)
-		{
-			if (player.pos_x > 1)
-				player.pos_x--;
-			else if (player.pos_x <= 1)
-				player.pos_x = SCR_WIDTH;
-		}
-
-		if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
-		{
-			if (player.pos_x < SCR_WIDTH - player.width - 1)
-				player.pos_x++;
-			else if (player.pos_x >= SCR_WIDTH - player.width - 1)
-				player.pos_x = 1;
-		}
-
-		if (GetAsyncKeyState(VK_DOWN) & 0x8000)
-		{
-			if (player.pos_y > 0)
-				player.pos_y--;
-		}
-
-		if (GetAsyncKeyState(VK_UP) & 0x8000)
-		{
-			if (player.pos_y < SCR_HEIGHT - 1)
-				player.pos_y++;
-		}
-
-		static bool roaded = true;
-
-		if (GetAsyncKeyState(VK_SPACE) & 0x8000)
-		{
-			if (roaded == true)
-			{
-				shootBullet();
-				roaded = false;
-			}
-		}
-
-		else
-		{
-			roaded = true;
-		}
-
-		for (int b = 0; b < MAX_BULLETS; b++)
-		{
-			if (p_bullet_array[b] != NULL)
-			{
-				if (p_bullet_array[b]->pos_y > SCR_HEIGHT - 1)
-				{
-					free(p_bullet_array[b]);
-					p_bullet_array[b] = NULL;
-				}
-				else
-					p_bullet_array[b]->pos_y++;
-			}
-		}
-
-		const int dice = rand() % 7; //0:stay, 1:right, 2:left
-		switch (dice)
-		{
-		case 1:  //right
-			if (enemy.pos_x < SCR_WIDTH - enemy.width - 1)
-				enemy.pos_x++;
-			else if (enemy.pos_x >= SCR_WIDTH - enemy.width - 1)
-				enemy.pos_x = 1;
-			break;
-
-		case 2:  //left
-			if (enemy.pos_x > 1)
-				enemy.pos_x--;
-			else if (enemy.pos_x <= 1)
-				enemy.pos_x = SCR_WIDTH;
-			break;
-		}
-
-		if (enemy.state > 0)
-			enemy.state--;
-
-		for (int b = 0; b < MAX_BULLETS; b++)
-		{
-			if (p_bullet_array[b] != NULL)
-			{
-				//check collision
-				if (p_bullet_array[b]->pos_y == enemy.pos_y)
-					if (p_bullet_array[b]->pos_x >= enemy.pos_x&&
-						p_bullet_array[b]->pos_x <= enemy.pos_x + enemy.width - 1)
-					{
-						score++;
-						enemy.state = 15;
-					}
-			}
-		}
-
-		drawAll();
-
-		render();
-
-		Sleep(30);
+		x_data[i] = (double)(rand() % 4);
+		y_data[i] = 3*x_data[i] +1+(double)(rand()/RAND_MAX);
 	}
 
-	for (int b = 0; b < MAX_BULLETS; b++)
-	{
-		if(p_bullet_array[b]!=NULL)
-			free(p_bullet_array[b]);
-	}
-}
+	const double learning_rate = 0.00001;
 
-
-
-
-
-
-void moveCursorTo(const short x, const short y)
-{
-	const COORD pos = { x,SCR_HEIGHT - y - 1 };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-}
-
-void drawToBackBuffer(const int i, const int j, char* image)
-{
-	int ix = 0;
-	while (1)
-	{
-		if (image[ix] == '\0') break;
-
-		back_buffer[j][i + ix] = image[ix];
-
-		ix++;
-	}
-}
-
-void render()
-{
-	//re-draw changed parts
-
-	for (int j = 0; j < SCR_HEIGHT; j++)
-		for (int i = 0; i < SCR_WIDTH; i++)
+	for (int tr = 0; tr < 10000; tr++)
+	{	
+		for (int i = 0; i < random_data; i++)
 		{
-			if (back_buffer[j][i] != front_buffer[j][i])
-			{
-				moveCursorTo(i, j);
+			const double error = y_data[i] - lh.GetY(x_data[i]);
+			const double square_error = 0.5*error*error;
 
-				if (back_buffer[j][i] == '\0')
-					printf("%c", ' ');
-				else
-					printf("%c", back_buffer[j][i]);
-			}
-		}
+			//se=(y_target-a*x-b)^2
+			const double dse_da = 2 * error*-x_data[i];
+			const double dse_db = 2 * error*-1;
 
-	//update frame buffer
-	for (int j = 0; j < SCR_HEIGHT; j++)
-		for (int i = 0; i < SCR_WIDTH; i++)
-		{
-			front_buffer[j][i] = back_buffer[j][i];
-			back_buffer[j][i] = '\0';
+			lh.a_ -= dse_da*learning_rate;
+			lh.b_ -= dse_db*learning_rate;
 		}
-}
-
-void shootBullet()
-{
-	int b_ix = -1;
-	for (int b = 0; b < MAX_BULLETS; b++)
-	{
-		if (p_bullet_array[b] == NULL)
-		{
-			b_ix = b;
-			break;
-		}
+		if (tr % 2000 == 0)
+			cout << lh.a_ << "   " << lh.b_ << endl << endl;
 	}
 
-	if (b_ix == -1) return;
+	cout << y_data[7] << "   " << lh.GetY(x_data[7]) << endl;
+	cout << endl << lh.a_ << "    " << lh.b_ << endl;
 
-	p_bullet_array[b_ix] = (Object*)malloc(sizeof(Object));
-
-	p_bullet_array[b_ix]->state = 0;
-	p_bullet_array[b_ix]->width = 1;
-	p_bullet_array[b_ix]->height = 1;
-	p_bullet_array[b_ix]->pos_x = player.pos_x + 2;
-	p_bullet_array[b_ix]->pos_y = player.pos_y;
-	p_bullet_array[b_ix]->image_ = "!\0!\0^\0^\0";
-
-}
-
-void drawBoundary()
-{
-	int i = 0;
-	for (int j = 0; j < SCR_HEIGHT; j++)
-	{
-		drawToBackBuffer(i, j, "|");
-	}
-
-	i = SCR_WIDTH - 1;
-	for (int j = 0; j < SCR_HEIGHT; j++)
-	{
-		drawToBackBuffer(i, j, "|");
-	}
-}
-
-void drawAll()
-{
-
-	//draw Boundary
-	drawBoundary();
-
-	//draw Character
-
-	drawToBackBuffer(player.pos_x, player.pos_y, player.image_);
-
-	//draw Enemy
-	char *current_state_image = enemy.image_ + (enemy.width + 1)*enemy.state;  //+1 is for null character
-
-	drawToBackBuffer(enemy.pos_x, enemy.pos_y, current_state_image);
-
-	//draw Bullet
-	for (int b = 0; b < MAX_BULLETS; b++)
-	{
-		if (p_bullet_array[b] != NULL)
-		{
-			char *current_bullet_image = p_bullet_array[b]->image_ +
-				(p_bullet_array[b]->width + 1)*p_bullet_array[b]->state;
-
-			drawToBackBuffer(p_bullet_array[b]->pos_x, p_bullet_array[b]->pos_y, current_bullet_image);
-
-			p_bullet_array[b]->state = (p_bullet_array[b]->state + 1) % 4;
-		}
-	}
-
-	//draw Score	
-	char scoretext[15];
-	sprintf(scoretext, "Score:%d", score);
-	drawToBackBuffer(15, SCR_HEIGHT - 1, scoretext);
-
+	delete[] x_data;
+	delete[] y_data;
+	return 0;
 }
---
+++ ConsoleApplication6/ConsoleApplication6/source.cpp
@@ -0,0 +1,185 @@
+#include <iostream>
+
+#define MAX2(a,b)              ((a)>(b) ? (a):(b))
+#define MAX3(a,b,c)            MAX2(MAX2(a,b),(c))
+#define MAX4(a,b,c,d)          MAX2(MAX3(a,b,c),(d))
+using namespace std;
+
+class CellData
+{
+public:
+	double q_[4] = { 0.0, 0.0, 0.0, 0.0 };  //up, down, left, right
+	double reward = 0.0;
+
+	CellData()
+	{}
+
+	double GetMaxQ()
+	{
+		return MAX4(q_[0], q_[1], q_[2], q_[3]);
+	}
+};
+
+class GridWorld    //To Do: make destructor -> it is necessary?
+{
+public:
+	int i_num, j_num;
+	CellData *q_arr2d = nullptr;
+
+	GridWorld(const int& i_res, const int& j_res)
+		:i_num(i_res), j_num(j_res)
+	{
+		q_arr2d = new CellData[i_res*j_res];
+	}
+
+	CellData& GetCellData(const int& i, const int& j)
+	{
+		return q_arr2d[i + i_num*j];
+	}
+
+	bool IsInsideGrid(const int& i, const int& j)
+	{
+		if (i > i_num) return false;
+		if (j > j_num) return false;
+		if (i < 0) return false;
+		if (j < 0) return false;
+		//if (i == 2 && j == 0) return false;	
+		//if (i == 2 && j == 1) return true;
+		return true;
+	}
+	
+	void printSigned(const float& v)
+	{
+		if (v > 0.0f) printf("+%1.1f",v );
+		else if (v < 0.0f) printf("+%1.f", v);
+		else
+			printf(" 0.0");
+	}
+
+	void print()
+	{
+		for (int j = j_num - 1; j >= 0; j--)       //i_num=3, j_num=2
+		{
+			for (int i = 0; i < i_num; i++)
+			{
+				CellData &cell = GetCellData(i, j);
+
+				printf("    "); printSigned(cell.q_[0]); printf("    ");  //up
+				printf("    ");
+			}
+
+			printf("\n");
+
+			for (int i = 0; i < i_num; i++)
+			{
+				CellData &cell = GetCellData(i, j);
+
+				printSigned(cell.q_[2]); printf("    "); printSigned(cell.q_[3]);  //left, right
+				printf("    ");
+			}
+
+			printf("\n");
+
+			for (int i = 0; i < i_num; i++)
+			{
+				CellData &cell = GetCellData(i, j);
+
+				printf("    "); printSigned(cell.q_[1]); printf("    ");   //down
+				printf("    ");
+			}
+			printf("\n\n");
+		}
+		printf("\n\n\n");
+	}
+};
+
+class Agent
+{
+public:
+	int i_, j_;     //position(st)
+//	double reward;
+
+	Agent()
+		:i_(0), j_(0)
+	{ }
+};
+
+int main()
+{
+	cout << "Hello GridWorld!" << endl << endl;
+	const int i_res = 3, j_res = 2;
+
+	GridWorld world(i_res, j_res);
+	CellData data;
+
+	for (int j = 0; j < j_res; j++)
+		for (int i = 0; i < i_res; i++)
+		{
+			world.GetCellData(i, j).reward = -0.1;   //밑에 2줄까지 합쳐서 reward칸외엔 보상이 -0.1
+		}
+	world.GetCellData(2, 1).reward = 1.0;
+	world.GetCellData(2, 0).reward = -1.0;
+	
+	Agent my_agent;
+
+	world.print();
+
+	for (int t = 0; t < 1000; t++)
+	{
+		const int action = rand() % 4;
+		int i = my_agent.i_, j = my_agent.j_;   //i=0, my_agent.i_=0
+		int i_old = i, j_old = j;               //i_old=0, j_old=0
+		
+		//Q(s_t,a_t)=Q(s_t,a_t)+ lr*((r_t+1)+df*maxQ_a(s_t+1, a)-Q(s_t,a_t))
+		float learning_rate = 0.00001f;
+		float discount_factor = 0.9f;
+
+		switch (action)       //move agent
+		{
+		case 0:
+			my_agent.j_++;
+			break; //up
+		case 1:
+			my_agent.j_--;
+			break;  //down
+		case 2:
+			my_agent.i_--;
+			break;   //left
+		case 3:
+			my_agent.i_++;
+			break;   //right
+		}
+
+		if (world.IsInsideGrid(i,j) == true)
+		{
+		
+			//move agent
+
+			//update reward(r_t) -> ????????
+
+
+			world.GetCellData(i_old ,j_old).q_[action] += learning_rate*(world.GetCellData(my_agent.i_, my_agent.j_).reward+ discount_factor*MAX4(world.GetCellData(i_old, j_old).q_[0], world.GetCellData(i_old, j_old).q_[1], world.GetCellData(i_old, j_old).q_[2], world.GetCellData(i_old, j_old).q_[3]) - world.GetCellData(i_old, j_old).q_[action]);
+			//update q values of previous cell (q_t)
+
+			if (my_agent.i_ == 2)
+			{
+				my_agent.i_ == 0;
+				my_agent.j_ == 0;
+			}
+			//reset if agent is in final cells
+		}
+		else
+		{
+			//you may give negative reward(penalty) to agent
+		}
+
+		//cout << "Agent status: " << my_agent.i_ << "  " << my_agent.j_ << endl;
+		//cout << "action " << action << endl;
+
+		//world.print();
+	}
+
+	world.print();
+
+	return 0;
+}
