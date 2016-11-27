915962ba8f0ce21279ae52f413346707c49d89d7
유동호 <deaocis@gmail.com>
2016-11-28 01:17:26 +0900

    whywhy

--- My_Own/My_Own/source.cpp
+++ My_Own/My_Own/source.cpp
@@ -8,8 +8,8 @@
 #define MAX3(a,b,c)            MAX2(MAX2(a,b),(c))
 #define MAX4(a,b,c,d)          MAX2(MAX3(a,b,c),(d))
 
-#define SCR_WIDTH 45   
-#define SCR_HEIGHT 20
+#define SCR_WIDTH 10   
+#define SCR_HEIGHT 8
 #define MAX_BULLETS 5
 #define MAX_E_BULLETS 4
 #define POWER_BULLETS 2
@@ -102,10 +102,10 @@ void Gameover();
 void render();	//바뀐부분 다시 그리기
 void drawBoundary();       //경계선 긋기
 
-void main()
+int main()
 {
 	my_player player, reward;
-	enemy enemy, enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8, enemy9;
+	enemy enemy, enemy1, enemy2, enemy3/*, enemy4, enemy5, enemy6, enemy7, enemy8, enemy9*/;
 	for (int j = 0; j < SCR_HEIGHT; j++)
 		for (int i = 0; i < SCR_WIDTH; i++)
 		{
@@ -123,38 +123,38 @@ void main()
 	player.pos_y_ = 1;
 
 	reward.image_ = "!";
-	reward.pos_x_ = 43;
-	reward.pos_y_ = 19;
+	reward.pos_x_ = 9;
+	reward.pos_y_ = 7;
 
-	enemy.pos_x_ = 10;
-	enemy.pos_y_ = 5;
+	enemy.pos_x_ = 3;
+	enemy.pos_y_ = 3;
 
-	enemy1.pos_x_ = 10;
-	enemy1.pos_y_ = 15;
+	//enemy1.pos_x_ = 4;
+	//enemy1.pos_y_ = 3;
 
-	enemy2.pos_x_ = 20;
-	enemy2.pos_y_ = 8;
+	//enemy2.pos_x_ = 5;
+	//enemy2.pos_y_ = 3;
 
-	enemy3.pos_x_ = 30;
-	enemy3.pos_y_ = 11;
+	//enemy3.pos_x_ = 6;
+	//enemy3.pos_y_ = 2;
 
-	enemy4.pos_x_ = 17;
-	enemy4.pos_y_ = 4;
+	//enemy4.pos_x_ = 17;
+	//enemy4.pos_y_ = 4;
 
-	enemy5.pos_x_ = 24;
-	enemy5.pos_y_ = 10;
+	//enemy5.pos_x_ = 24;
+	//enemy5.pos_y_ = 10;
 
-	enemy6.pos_x_ = 28;
-	enemy6.pos_y_ = 2;
+	//enemy6.pos_x_ = 28;
+	//enemy6.pos_y_ = 2;
 
-	enemy7.pos_x_ = 5;
-	enemy7.pos_y_ = 16;
+	//enemy7.pos_x_ = 5;
+	//enemy7.pos_y_ = 16;
 
-	enemy8.pos_x_ = 37;
-	enemy8.pos_y_ = 5;
+	//enemy8.pos_x_ = 37;
+	//enemy8.pos_y_ = 5;
 
-	enemy9.pos_x_ = 25;
-	enemy9.pos_y_ = 12;
+	//enemy9.pos_x_ = 25;
+	//enemy9.pos_y_ = 12;
 
 	const int i_res = SCR_WIDTH, j_res = SCR_HEIGHT;
 
@@ -169,18 +169,18 @@ void main()
 		}
 	}
 
-	world.GetCellData(43, 19).reward = 1.0;
+	world.GetCellData(9, 7).reward = 1.0;
 
-	world.GetCellData(10, 5).reward = -1.0;
-	world.GetCellData(10, 15).reward = -1.0;
-	world.GetCellData(20, 8).reward = -1.0;
-	world.GetCellData(30, 11).reward = -1.0;
-	world.GetCellData(17, 4).reward = -1.0;
-	world.GetCellData(24, 10).reward = -1.0;
-	world.GetCellData(28, 2).reward = -1.0;
-	world.GetCellData(5, 16).reward = -1.0;
-	world.GetCellData(37, 5).reward = -1.0;
-	world.GetCellData(25, 12).reward = -1.0;
+	world.GetCellData(3, 3).reward = -1.0;
+	//world.GetCellData(4, 3).reward = -1.0;
+	//world.GetCellData(5, 3).reward = -1.0;
+	//world.GetCellData(6, 2).reward = -1.0;
+	//world.GetCellData(17, 4).reward = -1.0;
+	//world.GetCellData(24, 10).reward = -1.0;
+	//world.GetCellData(28, 2).reward = -1.0;
+	//world.GetCellData(5, 16).reward = -1.0;
+	//world.GetCellData(37, 5).reward = -1.0;
+	//world.GetCellData(25, 12).reward = -1.0;
 
 	for (int tr = 0; tr < 100000; tr++)
 	{
@@ -190,7 +190,7 @@ void main()
 		int i_old = i;
 		int j_old = j;               //i_old=0, j_old=0
 
-		float learning_rate = 0.01f;
+		float learning_rate = 0.5f;
 		float discount_factor = 0.9f;
 
 		switch (action)       //move player
@@ -230,23 +230,27 @@ void main()
 			world.GetCellData(i_old, j_old).q_[action] += learning_rate*(world.GetCellData(player.pos_x_, player.pos_y_).reward + discount_factor*MAX4(world.GetCellData(player.pos_x_, player.pos_y_).q_[0], world.GetCellData(player.pos_x_, player.pos_y_).q_[1], world.GetCellData(player.pos_x_, player.pos_y_).q_[2], world.GetCellData(player.pos_x_, player.pos_y_).q_[3]) - world.GetCellData(i_old, j_old).q_[action]);
 			//update q values of previous cell (q_t)
 
-			if ((player.pos_x_ == 43 && player.pos_y_ == 19) || (player.pos_x_ == 10 && player.pos_y_ == 5) || (player.pos_x_ == 10 && player.pos_y_ == 15) || (player.pos_x_ == 20 && player.pos_y_ == 8) || (player.pos_x_ == 30 && player.pos_y_ == 11) || (player.pos_x_ == 17 && player.pos_y_ == 4) || (player.pos_x_ == 24 && player.pos_y_ == 10) || (player.pos_x_ == 28 && player.pos_y_ == 2) || (player.pos_x_ == 5 && player.pos_y_ == 16) || (player.pos_x_ == 37 && player.pos_y_ == 5) || (player.pos_x_ == 25 && player.pos_y_ == 12))
+			if ((player.pos_x_ == 9 && player.pos_y_ == 7) || (player.pos_x_ == 3 && player.pos_y_ == 3) /*|| (player.pos_x_ == 4 && player.pos_y_ == 3) || (player.pos_x_ == 5 && player.pos_y_ == 3) || (player.pos_x_ == 6 && player.pos_y_ == 2)*//* || (player.pos_x_ == 17 && player.pos_y_ == 4) || (player.pos_x_ == 24 && player.pos_y_ == 10) || (player.pos_x_ == 28 && player.pos_y_ == 2) || (player.pos_x_ == 5 && player.pos_y_ == 16) || (player.pos_x_ == 37 && player.pos_y_ == 5) || (player.pos_x_ == 25 && player.pos_y_ == 12)*/)
 			{
 				player.pos_x_ = 1;
 				player.pos_y_ = 1;
 			}
 			//reset if agent is in final cells
 		}
+		//cout << world.GetCellData(10,10).q_[0] << endl;
+		//cout << world.GetCellData(10, 10).q_[1] << endl;
+		//cout << world.GetCellData(10, 10).q_[2] << endl;
+		//cout << world.GetCellData(10, 10).q_[3] << endl << endl;
 	}
-	player.pos_x_ = 10; player.pos_y_ = 10;
+
+	player.pos_x_ = 5; player.pos_y_ = 3;
+	int tr = 0;
 	while (true)                  // main game loop
 	{
-		
+		double result = 0.0;
 		if (world.IsInsideGrid(player.pos_x_, player.pos_y_) == true)
 		{
-			double result;
 			result = MAX4(world.GetCellData(player.pos_x_, player.pos_y_).q_[0], world.GetCellData(player.pos_x_, player.pos_y_).q_[1], world.GetCellData(player.pos_x_, player.pos_y_).q_[2], world.GetCellData(player.pos_x_, player.pos_y_).q_[3]);
-
 			if (result == world.GetCellData(player.pos_x_, player.pos_y_).q_[0])
 			{
 				player.pos_y_++;
@@ -267,58 +271,54 @@ void main()
 				player.pos_x_++;
 			}
 
-			if (player.pos_x_ == 43 && player.pos_y_ == 19)
+			if ((player.pos_x_ == 9 && player.pos_y_ == 7))
 			{
 				Gameover();
+				return 0;
 			}
 		}
 
-		//1. q[0], q[1], q[2], q[3]중에 큰 쪽으로 이동   ->q[0]=up , q[1]=down,  q[2]=left, q[3]=right
-		//2. (73,19)에 이동하면 
-		//3. return 0;
-
 		//draw Boundary
-		drawBoundary();
+		//drawBoundary();
 
 		//draw player
 		char *current_state_p_image = player.image_ + (player.width_ + 1);
 		char *current_state_r_image = player.image_ + (player.width_ + 1);
 
 		char *current_state_e_image = enemy.image_ + (enemy.width_ + 1);
-		char *current_state_e1_image = enemy1.image_ + (enemy1.width_ + 1);
-		char *current_state_e2_image = enemy2.image_ + (enemy2.width_ + 1);
-		char *current_state_e3_image = enemy3.image_ + (enemy3.width_ + 1);
-		char *current_state_e4_image = enemy4.image_ + (enemy4.width_ + 1);
-		char *current_state_e5_image = enemy5.image_ + (enemy5.width_ + 1);
-		char *current_state_e6_image = enemy6.image_ + (enemy6.width_ + 1);
-		char *current_state_e7_image = enemy7.image_ + (enemy7.width_ + 1);
-		char *current_state_e8_image = enemy8.image_ + (enemy8.width_ + 1);
-		char *current_state_e9_image = enemy9.image_ + (enemy9.width_ + 1);
+		//char *current_state_e1_image = enemy1.image_ + (enemy1.width_ + 1);
+		//char *current_state_e2_image = enemy2.image_ + (enemy2.width_ + 1);
+		//char *current_state_e3_image = enemy3.image_ + (enemy3.width_ + 1);
+		//char *current_state_e4_image = enemy4.image_ + (enemy4.width_ + 1);
+		//char *current_state_e5_image = enemy5.image_ + (enemy5.width_ + 1);
+		//char *current_state_e6_image = enemy6.image_ + (enemy6.width_ + 1);
+		//char *current_state_e7_image = enemy7.image_ + (enemy7.width_ + 1);
+		//char *current_state_e8_image = enemy8.image_ + (enemy8.width_ + 1);
+		//char *current_state_e9_image = enemy9.image_ + (enemy9.width_ + 1);
 
 		drawToBackBuffer(player.pos_x_, player.pos_y_, player.image_);
 		drawToBackBuffer(reward.pos_x_, reward.pos_y_, reward.image_);
 
 		drawToBackBuffer(enemy.pos_x_, enemy.pos_y_, enemy.image_);
-		drawToBackBuffer(enemy1.pos_x_, enemy1.pos_y_, enemy1.image_);
-		drawToBackBuffer(enemy2.pos_x_, enemy2.pos_y_, enemy2.image_);
-		drawToBackBuffer(enemy3.pos_x_, enemy3.pos_y_, enemy3.image_);
-		drawToBackBuffer(enemy4.pos_x_, enemy4.pos_y_, enemy4.image_);
-		drawToBackBuffer(enemy5.pos_x_, enemy5.pos_y_, enemy5.image_);
-		drawToBackBuffer(enemy6.pos_x_, enemy6.pos_y_, enemy6.image_);
-		drawToBackBuffer(enemy7.pos_x_, enemy7.pos_y_, enemy7.image_);
-		drawToBackBuffer(enemy8.pos_x_, enemy8.pos_y_, enemy8.image_);
-		drawToBackBuffer(enemy9.pos_x_, enemy9.pos_y_, enemy9.image_);
+		//drawToBackBuffer(enemy1.pos_x_, enemy1.pos_y_, enemy1.image_);
+		//drawToBackBuffer(enemy2.pos_x_, enemy2.pos_y_, enemy2.image_);
+		//drawToBackBuffer(enemy3.pos_x_, enemy3.pos_y_, enemy3.image_);
+		//drawToBackBuffer(enemy4.pos_x_, enemy4.pos_y_, enemy4.image_);
+		//drawToBackBuffer(enemy5.pos_x_, enemy5.pos_y_, enemy5.image_);
+		//drawToBackBuffer(enemy6.pos_x_, enemy6.pos_y_, enemy6.image_);
+		//drawToBackBuffer(enemy7.pos_x_, enemy7.pos_y_, enemy7.image_);
+		//drawToBackBuffer(enemy8.pos_x_, enemy8.pos_y_, enemy8.image_);
+		//drawToBackBuffer(enemy9.pos_x_, enemy9.pos_y_, enemy9.image_);
 
 		Sleep(50);
 
 		render();
 	}
 	//main loop END
+
+	return 0;
 }
 
-//일단 player가 안움직임  ->근데 왜 0,0으로 가지
-//예상 이유 1. 정해진 범위 밖으로 나감
-//			2. ..
 
 
 
